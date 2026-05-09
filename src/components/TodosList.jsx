7e15ece99d864f33b2cc646f32ff0f4f41d41038
ivo-petronio <scytheratk@gmail.com>
2026-05-09 14:25:14 -0300

    feat: applying useState

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,6 +1,15 @@
 import Todo from './Todo.jsx';
+import { useState } from 'react'
+
+const initialTodos = [
+  { id: 0, title: 'Do Groceries', description: 'Buy apples, rice, juice and toilet paper.', isDone: true },
+  { id: 1, title: 'Study React', description: 'Understand context & reducers.', isDone: false},
+  { id: 2, title: 'Learn Redux', description: 'Learn state management with Redux', isDone: false }
+];
 
 function TodosList() {
+
+  const [tasks, setTasks] = useState(initialTodos)
 
   return (
     <>
